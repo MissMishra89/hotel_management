87671214517bc54921b9889f0c31cc4f616d832d
Khushi Mishra <misskhushi597@gmail.com>
2026-04-16 17:30:14 +0530

    Practice1

--- index.mjs
+++ index.mjs
@@ -1,5 +1,17 @@
 import express from "express";
 const app = express();
-app.listen(5000, () => {
-  console.log("server started on port 5000");
+const port = 5000;
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.json({ greet: "hello" });
+});
+app.post("/login", (req, res) => {
+  console.log(req.body);
+  res.json({ greet: "hello" });
+});
+app.post("/signup", (req, res) => {
+  res.json({ name: "khushi" });
+});
+app.listen(port, () => {
+  console.log(`server started on ${port}`);
 });
